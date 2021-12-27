6277e92e726d976c279b0103d769fbbccf2a5a0d
Ras-AlGhoul <yazann.abbas@gmail.com>
2021-12-27 11:22:27 +0100

    fix bug

--- lib/db.ts
+++ lib/db.ts
@@ -2,12 +2,12 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAdFe32HGZG423dTYQ0sjeed8QUDv_0m2I',
-  authDomain: 'ubiquitiapp.firebaseapp.com',
-  projectId: 'ubiquitiapp',
-  storageBucket: 'ubiquitiapp.appspot.com',
-  messagingSenderId: '445787014203',
-  appId: '1:445787014203:web:167b3606f3f3d6f278481e',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 if (!firebase.apps.length) {
