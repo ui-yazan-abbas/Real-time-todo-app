51e4110bdf698e71279421f976d35b05407a7f7c
Ras-AlGhoul <yazann.abbas@gmail.com>
2022-01-02 10:53:57 +0100

    add precentage instead of coordinations

--- components/CollaberatorLayer.tsx
+++ components/CollaberatorLayer.tsx
@@ -1,27 +1,36 @@
-import { FC } from 'react';
+import { FC, RefObject, useEffect, useState } from 'react'
 import { Box } from 'theme-ui';
-import { Plus } from '@assets/icons';
+import { Cross } from '@assets/icons';
+
 
 const CollaberatorLayer: FC<{
-  collaborators?: Record<string, { x: number; y: number }>;
-}> = ({ collaborators }) => {
-  const mice = Object.values(collaborators || {});
+  cardRef: RefObject<HTMLDivElement>
+  collaborators?: Record<string, { x: number; y: number }>
+}> = ({ collaborators, cardRef }) => {
+  const mice = Object.values(collaborators || {})
+  const [rect, setRect] = useState({ width: 0, height: 0 })
+  useEffect(() => {
+    if (cardRef.current) {
+      const { width, height } = cardRef.current.getBoundingClientRect()
+      setRect({ width, height })
+    }
+  }, [cardRef.current])
   return (
-    <Box>
+    <>
       {mice.map(({ x, y }) => (
         <Box
           key={`${x}:${y}`}
           sx={{
             position: 'absolute',
-            top: y,
-            left: x,
+            top: y * rect.height,
+            left: x * rect.width,
           }}
         >
-          <Plus />
+          <Cross />
         </Box>
       ))}
-    </Box>
-  );
-};
+    </>
+  )
+}
 
 export default CollaberatorLayer;
--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -1,23 +1,21 @@
-import { Cross } from '@assets/icons';
-import { FC } from 'react';
-import { Switch, Box, Card, IconButton } from 'theme-ui';
-import RichTextEditor from './RichTextEditor';
-import { throttle } from 'lodash';
-import CollaberatorLayer from './CollaberatorLayer';
-import { Todo } from '@utils/types';
+import { Cross } from '@assets/icons'
+import { FC, RefObject, useEffect, useRef, useState, useMemo } from 'react'
+import { Switch, Box, Card, IconButton } from 'theme-ui'
+import RichTextEditor from './RichTextEditor'
+import { throttle } from 'lodash'
+import { Todo } from '@utils/types'
 import Link from 'next/link';
-import { WhatsappShareButton, FacebookShareButton, EmailShareButton, LinkedinShareButton } from 'react-share';
-import { FacebookIcon, WhatsappIcon, EmailIcon, LinkedinIcon } from 'react-share';
-import { FacebookMessengerShareButton } from 'react-share';
 import ShareLink from './ShareLink';
+import CollaberatorLayer from './CollaberatorLayer';
+
 
 interface Props {
-  todo: Todo;
-  onUpdate: (updatedTodo: Todo) => void;
-  onDelete: (todo: Todo) => void;
-  onMouseOver: (cords: { x: number; y: number }) => void;
-  onMouseLeave: () => void;
-  collaborators?: Record<string, { x: number; y: number }>;
+  todo: Todo
+  onUpdate: (updatedTodo: Todo) => void
+  onDelete: (todo: Todo) => void
+  onMouseOver: (cords: { x: number; y: number }) => void
+  onMouseLeave: () => void
+  collaborators?: Record<string, { x: number; y: number }>
 }
 
 const TodoCard: FC<Props> = ({
@@ -28,17 +26,39 @@ const TodoCard: FC<Props> = ({
   onMouseLeave,
   collaborators,
 }) => {
+  const cardRef = useRef<HTMLDivElement>(null)
+  const [backgroundColor, setBackgroundColor] = useState('')
+  const broadcastMousLocation = useMemo(
+    () =>
+      throttle((event: React.MouseEvent<HTMLDivElement>) => {
+        const rect = cardRef.current?.getBoundingClientRect()
+        if (rect) {
+          const x = (event.clientX - rect.left) / rect.width //x position within the element.
+          const y = (event.clientY - rect.top) / rect.height //y position within the element.
+          onMouseOver({
+            x,
+            y,
+          })
+        }
+      }, 150),
+    [cardRef.current]
+  )
+
+  useEffect(() => {
+    const hasCollaborators = Object.keys(collaborators || {}).length > 0
+    if (hasCollaborators) {
+      setBackgroundColor('lightBlue')
+    } else {
+      setBackgroundColor(todo.completed ? '#cfc' : 'yellow')
+    }
+  }, [collaborators, todo])
   return (
     <Card
-      onMouseOver={throttle((event: React.MouseEvent<HTMLDivElement>) => {
-        onMouseOver({
-          x: event.clientX,
-          y: event.clientY,
-        });
-      }, 5)}
-      onMouseLeave={throttle(() => onMouseLeave())}
-      bg={todo.completed ? '#cfc' : 'yellow'}
-      p={4}
+      ref={cardRef}
+      onMouseMove={broadcastMousLocation}
+      onMouseLeave={throttle(onMouseLeave, 100, { leading: false })}
+      bg={backgroundColor}
+      p={3}
       m={4}
       sx={{
         boxShadow: 'none',
@@ -49,13 +69,16 @@ const TodoCard: FC<Props> = ({
         },
       }}
     >
-      <CollaberatorLayer collaborators={collaborators} />
+      <CollaberatorLayer
+        cardRef={cardRef}
+        collaborators={collaborators}
+      />
       <IconButton
         sx={{ position: 'absolute', top: 10, right: 10 }}
         onClick={async () => {
           // todo loading indicator
           // todo test if current user can delete
-          onDelete(todo);
+          onDelete(todo)
         }}
       >
         <Cross />
@@ -75,7 +98,7 @@ const TodoCard: FC<Props> = ({
               onUpdate({
                 ...todo,
                 description: newDescription,
-              });
+              })
             }}
             value={todo.description}
           />
@@ -88,14 +111,44 @@ const TodoCard: FC<Props> = ({
               onUpdate({
                 ...todo,
                 completed: event.target.checked,
-              });
+              })
             }}
           ></Switch>
         </Box>
       </Box>
       <ShareLink id={todo.id}/>
     </Card>
-  );
-};
+  )
+}
 
-export default TodoCard;
+const CollaboratorsLayer: React.FC<{
+  cardRef: RefObject<HTMLDivElement>
+  collaborators?: Record<string, { x: number; y: number }>
+}> = ({ collaborators, cardRef }) => {
+  const mice = Object.values(collaborators || {})
+  const [rect, setRect] = useState({ width: 0, height: 0 })
+  useEffect(() => {
+    if (cardRef.current) {
+      const { width, height } = cardRef.current.getBoundingClientRect()
+      setRect({ width, height })
+    }
+  }, [cardRef.current])
+  return (
+    <>
+      {mice.map(({ x, y }) => (
+        <Box
+          key={`${x}:${y}`}
+          sx={{
+            position: 'absolute',
+            top: y * rect.height,
+            left: x * rect.width,
+          }}
+        >
+          <Cross />
+        </Box>
+      ))}
+    </>
+  )
+}
+
+export default TodoCard
